90e034c49c8eeba7df5d403d42cb78b8040ca88c
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-11-09 18:57:03 +0000

    asd

--- main.cs
+++ main.cs
@@ -9,17 +9,25 @@ class Program
 {
     public static void Main(string[] args)
     {
+        double TAXRATE;
+        TAXRATE = 0.18;
+        
         //this function accept user input
-        int workedHours;
-        int hourlyRate;
+        double workedHours;
+        double hourlyRate;
+        double homePay;
+        double governmentTook;
 
         Console.WriteLine("This program gets user's home pay.");
         Console.WriteLine("Please enter your employment information.\n");
         Console.Write("Enter number of hours worked: "); workedHours = Convert.ToInt32(Console.ReadLine());
         Console.Write("Enter hourly rate: "); hourlyRate = Convert.ToInt32(Console.ReadLine());
 
+        governmentTook = (workedHours * hourlyRate) * TAXRATE;
+        homePay = (workedHours * hourlyRate) - governmentTook;
+
         //this function prints the result
-        Console.WriteLine("\n\nYour pay will be: " + ((workedHours * hourlyRate) - ((workedHours * hourlyRate) * 0.18)).ToString("0.00") + ". ");
-        Console.WriteLine("Your government took: " + ((workedHours * hourlyRate) * 0.18).ToString("0.00") + ". ");
+        Console.WriteLine("\nYour pay will be: " + homePay.ToString("0.00") + " $");
+        Console.WriteLine("Your government took: " + governmentTook.ToString("0.00") + " $");
     }
 }
